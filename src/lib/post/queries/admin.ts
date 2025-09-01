3ff8cb64f8b62b83481392ac6c62008f9622ae6d
Armando Ribamar <armando.ribamar@gmail.com>
2025-09-01 13:51:24 -0300

    feat: add API query functions for retrieving posts by ID and all posts for admin

--- src/lib/post/queries/admin.ts
+++ src/lib/post/queries/admin.ts
@@ -1,4 +1,6 @@
+import { PostModelFromApi } from '@/models/post/post-model';
 import { postRepository } from '@/repositories/post';
+import { authenticatedApiRequest } from '@/utils/authenticated-api-request';
 import { cache } from 'react';
 
 export const findAllPostsAdmin = cache(async () => {
@@ -7,4 +9,32 @@ export const findAllPostsAdmin = cache(async () => {
 
 export const findPostByIdAdmin = cache(async (id: string) => {
   return await postRepository.findById(id);
+});
+
+export const findPostByIdApiAdmin = cache(async (id: string) => {
+  const postResponse = await authenticatedApiRequest<PostModelFromApi>(
+    `/post/me/${id}`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      cache: 'no-store',
+    },
+  );
+
+  return postResponse;
+});
+
+export const findAllPostsApiAdmin = cache(async () => {
+  const postsResponse = await authenticatedApiRequest<PostModelFromApi[]>(
+    '/post/me',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      cache: 'no-store',
+    },
+  );
+
+  return postsResponse;
 });
